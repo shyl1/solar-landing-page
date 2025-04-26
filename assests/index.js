840ce62d5aa699999b150026f2cebd7313f0c278
shyl <sohaylae615@gmail.com>
2025-04-26 17:36:02 +0300

    file structure

--- assests/index.js
+++ assests/index.js
@@ -1,41 +1,41 @@
-//importing images from the assets folder
-import greenEnergy1 from './images/green-energy1.png'
-import greenEnergy2 from './images/green-energy2.png';
-import placeHolder from './images/placeHolder.jpg';
-import planningEnvironment from './images/planning-enviroment.png';
-import solarWithPhotovoltaic from './images/solar-with-photovoltaic.png';
-import solarPanlels from "./images/solarPanels.png";
-import windMill from './images/windmill.png';
-import solarWithEarth from './images/solarwithearth.png';
+// //importing images from the assets folder
+// import greenEnergy1 from './images/green-energy1.png'
+// import greenEnergy2 from './images/green-energy2.png';
+// import placeHolder from './images/placeHolder.jpg';
+// import planningEnvironment from './images/planning-enviroment.png';
+// import solarWithPhotovoltaic from './images/solar-with-photovoltaic.png';
+// import solarPanlels from "./images/solarPanels.png";
+// import windMill from './images/windmill.png';
+// import solarWithEarth from './images/solarwithearth.png';
 
 
-//importing SVG from the assets folder
-import solarCharging from './svg/solarCharging.svg';
-import marks from './svg/marks.svg';
-import navigationRight from './svg/navigationRight.svg';
-import navigationLeft from './svg/navigationleft.svg';
-import priceTag from './svg/price-tag.svg';
-import FastEnergy from "./svg/fast-energy.svg";
-import Enviroment from './svg/Enviroment.svg';
-import rightArrow from './svg/right-arrow.svg';
+// //importing SVG from the assets folder
+// import solarCharging from './svg/solarCharging.svg';
+// import marks from './svg/marks.svg';
+// import navigationRight from './svg/navigationRight.svg';
+// import navigationLeft from './svg/navigationleft.svg';
+// import priceTag from './svg/price-tag.svg';
+// import FastEnergy from "./svg/fast-energy.svg";
+// import Enviroment from './svg/Enviroment.svg';
+// import rightArrow from './svg/right-arrow.svg';
 
 
-//exporting images and SVGs 
-export {
-  greenEnergy1,
-  greenEnergy2,
-  placeHolder,
-  planningEnvironment,
-  solarWithPhotovoltaic,
-  solarPanlels,
-  windMill,
-  solarWithEarth,
-  solarCharging,
-  marks,
-  navigationRight,
-  navigationLeft,
-  priceTag,
-  FastEnergy,
-  Enviroment,
-  rightArrow,
-};
+// //exporting images and SVGs 
+// export {
+//   greenEnergy1,
+//   greenEnergy2,
+//   placeHolder,
+//   planningEnvironment,
+//   solarWithPhotovoltaic,
+//   solarPanlels,
+//   windMill,
+//   solarWithEarth,
+//   solarCharging,
+//   marks,
+//   navigationRight,
+//   navigationLeft,
+//   priceTag,
+//   FastEnergy,
+//   Enviroment,
+//   rightArrow,
+// };
